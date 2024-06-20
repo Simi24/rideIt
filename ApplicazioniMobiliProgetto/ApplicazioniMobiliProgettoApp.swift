5ccef05903c5ebc6964c2d325d09dc477a7a4126
Simone Paolo Petta <pettasimonepaolo@gmail.com>
2024-06-20 16:38:13 +0200

    Initial Commit

---
+++ ApplicazioniMobiliProgetto/ApplicazioniMobiliProgettoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicazioniMobiliProgettoApp.swift
+//  ApplicazioniMobiliProgetto
+//
+//  Created by Simone Paolo Petta on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ApplicazioniMobiliProgettoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApplicazioniMobiliProgetto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApplicazioniMobiliProgetto
+//
+//  Created by Simone Paolo Petta on 20/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
